882561b49ddb452e3889fa9cdccd7843225e2ae3
Rick Hall <rickhall2000@hotmail.com>
2014-04-17 16:34:56 -0400

    added some basic structure

--- src/chess_clock/core.cljs
+++ src/chess_clock/core.cljs
@@ -4,10 +4,39 @@
 
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:text "Hello world!"
+                      :white-time {:min 4 :sec 59}
+                      :black-time {:min 5 :sec 00}}))
+
+(defn minus-sec [time]
+  (let [min (:min time)
+        sec (:sec time)]
+    (if (zero? sec)
+      {:min (dec min) :sec 59}
+      {:min min :sec (dec sec)})))
+
+(defn time->string [time]
+  (let [min (:min time)
+        sec  (subs (str (:sec time) "0") 0 2)]
+    (str min ":" sec)))
+
+(defn draw-clock [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div #js {:className "clock"}
+               (dom/h2 nil (time->string app))))))
+
+(defn draw-board [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+               (dom/h2 nil (:text app))
+               (om/build draw-clock (:white-time app))
+               (om/build draw-clock (:black-time app))))))
 
 (om/root
-  (fn [app owner]
-    (dom/h1 nil (:text app)))
+  draw-board
   app-state
   {:target (. js/document (getElementById "app"))})
