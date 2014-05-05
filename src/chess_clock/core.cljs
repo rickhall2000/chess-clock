d0bec75538a89b61a50a28ceb2c70e7b47529e6c
Rick Hall <rickhall2000@hotmail.com>
2014-05-05 16:24:01 -0400

    think I need to move the control channels into component state

--- src/chess_clock/core.cljs
+++ src/chess_clock/core.cljs
@@ -2,13 +2,9 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [chan >! <! alts! timeout]]))
+            [cljs.core.async :refer [chan >! <! alts! timeout put!]]))
 
 (enable-console-print!)
-
-(def app-state (atom {:text "Hello world!"
-                      :white-time {:min 4 :sec 59}
-                      :black-time {:min 5 :sec 00}}))
 
 (defn minus-sec [time]
   (let [min (:min time)
@@ -17,13 +13,17 @@
       {:min (dec min) :sec 59}
       {:min min :sec (dec sec)})))
 
+(defn time->string [time]
+  (let [min (:min time)
+        sec  (subs (str (:sec time) "0") 0 2)]
+    (str min ":" sec)))
 
-(defn counter [clock control]
-  (let [out (chan)
-        clock-state (cycle [:off :on])]
-    (.log js/console "about to loop")
+(defn counter [app]
+  (let [clock-state (cycle [:off :on])
+        control (:control app)
+        time-left (:time app)]
     (go
-     (loop [time-left clock clock-state clock-state]
+     (loop [time-left time-left clock-state clock-state]
        (do
          (let [t (timeout 1000)
                [v c] (alts! [t control])]
@@ -31,48 +31,62 @@
             (and (= (first clock-state) :on)
                  (= c t))
             (do
-              (>! out time-left)
+              (om/transact! app :time (fn [_] time-left))
               (recur (minus-sec time-left) clock-state))
             (and (= (first clock-state) :off)
                  (= c control)
                  (= v :start))
+            (recur time-left (next clock-state))
+            (and (= (first clock-state) :on)
+                 (= c control)
+                 (= v :stop))
             (do
-              (.log js/console "starting up")
               (recur time-left (next clock-state)))
             (and (= c control)
                  (= c :end))
             (.log js/console "time at end: " time-left)
             :else
-            (recur time-left clock-state))))))
-    out))
+            (recur time-left clock-state))))))))
 
+(def app-state
+  (atom {:white-clock {:control (chan)
+                       :time {:min 2 :sec 30}
+                       :tag :white}
+         :black-clock {:control (chan)
+                       :time {:min 2 :sec 30}
+                       :tag :black}}))
 
-(defn time->string [time]
-  (let [min (:min time)
-        sec  (subs (str (:sec time) "0") 0 2)]
-    (str min ":" sec)))
-
-(defn clock-builder []
-  (let [control (chan)
-        clock (counter {:min 2 :sec 30} control)]
+(defn switch-clock [msg]
+  (let [wc (:control (:white-clock @app-state))
+        bc (:control (:black-clock @app-state))]
+    (.log js/console msg)
     (go
-     (>! control :start)
-     (loop []
-       (let [time (<! clock)]
-         (.log js/console (time->string time))
-         (if  (< (:min time) 2)
-           (>! control :end)
-           (recur)))))))
-
-(clock-builder)
-
+     (cond
+      (= msg :white)
+      (do
+        (>! wc :stop)
+        (>! bc :start))
+      (= msg :black)
+      (do
+        (>! wc :start)
+        (>! bc :stop))
+      (= msg :end)
+      (do
+        (>! wc :end)
+        (>! bc :end))))))
 
 (defn draw-clock [app owner]
   (reify
-    om/IRender
-    (render [this]
-      (dom/div #js {:className "clock"}
-               (dom/h2 nil (time->string app))))))
+    om/IInitState
+    (init-state [_]
+      {:counter (counter app)})
+    om/IRenderState
+    (render-state [this state]
+      (let [tag (:tag app)]
+        (dom/div #js {:className "clock"}
+                 (dom/h2 nil (time->string (:time app)))
+                 (dom/button #js {:onClick
+                                  (fn [e] (switch-clock tag))} "Move"))))))
 
 (defn draw-board [app owner]
   (reify
@@ -80,8 +94,9 @@
     (render [this]
       (dom/div nil
                (dom/h2 nil (:text app))
-               (om/build draw-clock (:white-time app))
-               (om/build draw-clock (:black-time app))))))
+               (dom/button #js {:onClick (fn [e] (switch-clock :end))} "End Game")
+               (om/build draw-clock (:white-clock app))
+               (om/build draw-clock (:black-clock app))))))
 
 (om/root
   draw-board
